4e598d965187b65a6801a488afd414d3b6a71fb9
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2021-12-09 15:53:02 -0800

    create message db step 1

--- nyvia380server/src/main/java/com/project/nyvia380server/common/chat/Message.java
+++ nyvia380server/src/main/java/com/project/nyvia380server/common/chat/Message.java
@@ -3,12 +3,14 @@ package com.project.nyvia380server.common.chat;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 import java.util.UUID;
 
-@Document("Messages")
+@Document(collection = "Messages")
+@TypeAlias("Messages")
 @Builder
 @Getter
 @AllArgsConstructor
@@ -17,13 +19,12 @@ public class Message {
 
     @Id
     private String id;
-    private final String content;
-    private final String  senderId;
-    private final String  recipientId;
-    private final Date messageTime;
-    private final MessageType messageType; // request type
-    private final MessageStatus messageStatus; // message status
-
+    private final String message;
+    private final String  sender;
+    private final String  receiver;
+//    private final Date messageTime;
+//    private final MessageType messageType; // request type
+//    private final MessageStatus messageStatus; // message status
 
 //    public Message(String htmlEscape) {
 //
