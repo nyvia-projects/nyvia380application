1b02f57eaff6092eb71d5d1e97c7b6fbce12c751
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2021-12-09 19:20:10 -0800

    fixes conflicts

--- nyvia380server/src/main/java/com/project/nyvia380server/common/chat/ChatMessageService.java
+++ nyvia380server/src/main/java/com/project/nyvia380server/common/chat/ChatMessageService.java
@@ -2,10 +2,13 @@ package com.project.nyvia380server.common.chat;
 
 import com.project.nyvia380server.common.user.UserService;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+import static com.project.nyvia380server.Nyvia380Application.messageDB;
 
 @Service
 @RequiredArgsConstructor
@@ -17,20 +20,6 @@ public class ChatMessageService {
     @Autowired
     private UserService userService;
 
-    private MessageRepository messageRepository;
-
-    private final ModelMapper modelMapper;
-
-
-    private MessageMetaData converToDTO (Message message) {
-        return modelMapper.map(message, MessageMetaData.MessageMetaDataBuilder.class).build();
-    }
-
-    private Message convertToEntity (MessageMetaData dto) {
-        return modelMapper.map(dto, Message.class);
-    }
-
-
     public MessageMetaData sendMessage (MessageMetaData message) throws Exception {
         return MessageMetaData.builder().build();
     }
@@ -38,15 +27,11 @@ public class ChatMessageService {
     public void sendMessageTo (MessageMetaData message, String userName) throws Exception {
         if (userService.findUserByAlias(userName).getAlias().equals(userName)) {
             simpMessagingTemplate.convertAndSend("/topic/chat/" + userName, message);
-            insertMessage(convertToEntity(message));
+            messageDB.addMessage(message);
         }
     }
 
-    public Message insertMessage(Message message) {
-        return messageRepository.insert(message);
-    }
-
-    public void fetchAllMessages (String userName) {
-        //TODO: fetch all messages for particular user.
+    public ArrayList<MessageMetaData> fetchAllMessages (String userName) {
+        return messageDB.getMessagesByAlias(userName);
     }
 }
